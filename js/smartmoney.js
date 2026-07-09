50b7c9c0b319d7b38be49ba5111f2692b0e42487
nezirhnd-svg <nezirhnd@gmail.com>
2026-07-09 20:41:33 +0300

    Update smartmoney.js

--- js/smartmoney.js
+++ js/smartmoney.js
@@ -1,1 +1,32 @@
+// ==========================
+// HNDai Smart Money Engine
+// ==========================
 
+// BOS (Break Of Structure)
+function detectBOS() {
+
+    if (candles.length < 30) {
+        return "Scanning...";
+    }
+
+    const lastClose = candles[candles.length - 1].close;
+
+    const highs = candles
+        .slice(-21, -1)
+        .map(c => c.high);
+
+    const lows = candles
+        .slice(-21, -1)
+        .map(c => c.low);
+
+    const highest = Math.max(...highs);
+    const lowest = Math.min(...lows);
+
+    if (lastClose > highest)
+        return "Bullish";
+
+    if (lastClose < lowest)
+        return "Bearish";
+
+    return "No Break";
+}
